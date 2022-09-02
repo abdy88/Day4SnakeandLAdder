2ec2204c7109f10c475e1922362e781aeea09e4c
aytech11 <aytech0110@gmail.com>
2022-09-02 15:25:48 +0530

    Welcome to Snake and Ladder Simulator

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package bl.com.day4practiceproblems;
+
+public class SnakeAndLadder {
+	
+	
+	
+	public static void main(String[] args) {
+	
+	System.out.println("---Welcome to Snake and Ladder Simulator---");
+	}
+
+}
