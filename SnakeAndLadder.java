748329eb8b654ca893387485e048779d1ae7ad67
aytech11 <aytech0110@gmail.com>
2022-09-03 22:16:36 +0530

    UC4 1 player snake and ladder simulator

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -12,7 +12,7 @@ public class SnakeAndLadder {
 	
 	 
 	Firstthrow(); 
-	 
+	oneplayersimulator();
 	 
 	}
 	
@@ -68,17 +68,103 @@ public class SnakeAndLadder {
 					System.out.println("position after first throw : " + position );
 					
 					
-					
-					
-					
-					
+						
 				}
 	
 	
-	
+					static void oneplayersimulator()
+					{   
+						
+						 
+						 int position=0;
+//						 short options= (short)(Math.random() * 3 + 1);
+//						 System.out.println("option is =" + options);
+						 final short noplay=1;
+						 final short ladder=2;
+						 final short snake=3;
+						 int countOfThrow=0;
+						 
+						
+						while (position<=100)
+						 {
+							 int dieThrow= (int)(Math.random() * 6 + 1);
+							 System.out.println("dieThrow is =" + dieThrow);
+							 countOfThrow ++;
+//							 System.out.println("total count of throw = " + countOfThrow );
+							 
+							 short options= (short)(Math.random() * 3 + 1);
+							 System.out.println("option is =" + options);
+							 
+							 
+							 switch (options)
+							 {
+							 
+							 case (noplay):
+								 {
+							 		 }
+							 break;
+						 
+							 case (ladder):
+								 {
+							 		position =(position + dieThrow);
+								 }
+							 break;
+							 
+							 case (snake):
+								 {
+							 		position= (position - dieThrow);
+							 		
+							 		if (position<0)
+							 		{
+							 			position =(position + dieThrow);
+							 			
+							 			}
+							 		 }
+								 break;
+					
+							 }
+							 
+							 
+						
+							 if (position==100)
+							 {
+							 System.out.println("Player reached position 100 and player wins !! ");
+							 System.out.println("position after die throw is "  + position);
+							 System.out.println("total count of throw = " + countOfThrow );
+
+							 break;
+							 }
+							 
+							 else if (position>100)
+							 {
+								 position=position-dieThrow;
+								 
+							 }
+							 
+//						 System.out.println("position after die throw is "  + position);
+//						 System.out.println("total count of throw = " + countOfThrow );
+
+						 }
+
+					}
+						
+						
+						
+						
+					}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 	
 	
 	
 	
 
-}
+
