b7d11e1ebee008cf30680470e23914dedde7fd62
aytech11 <aytech0110@gmail.com>
2022-09-03 21:43:07 +0530

    The player rolls the die to get a number between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -11,6 +11,9 @@ public class SnakeAndLadder {
 	System.out.println(	"Snake and Ladder game played with single player at start position 0");
 	
 	int position=0;
+	
+	int dieThrow= (int)(Math.random() * 6 + 1);
+	System.out.println("dieThrow is =" + dieThrow);
 	 
 	 
 	}
