f590ee608d7d9aa6be8a6e5b0fcc91418ee056c1
aytech11 <aytech0110@gmail.com>
2022-09-03 22:01:40 +0530

    UC3 First throw of dice along with options and change in position

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,12 +10,75 @@ public class SnakeAndLadder {
 	
 	System.out.println(	"Snake and Ladder game played with single player at start position 0");
 	
-	int position=0;
-	
-	int dieThrow= (int)(Math.random() * 6 + 1);
-	System.out.println("dieThrow is =" + dieThrow);
+	 
+	Firstthrow(); 
 	 
 	 
 	}
+	
+	
+				static void Firstthrow() 
+				{
+					 
+					 int position=0;
+//					 short options= (short)(Math.random() * 3 + 1);
+//					 System.out.println("option is =" + options);
+					 final short noplay=1;
+					 final short ladder=2;
+					 final short snake=3;
+					 int countOfThrow=0;
+					 
+					int dieThrow= (int)(Math.random() * 6 + 1);
+					 System.out.println("dieThrow is =" + dieThrow);
+					 countOfThrow ++;
+//					 System.out.println("total count of throw = " + countOfThrow );
+					 
+					 short options= (short)(Math.random() * 3 + 1);
+					 System.out.println("option is =" + options);
+					 
+					 
+					 switch (options)
+					 {
+					 
+					 case (noplay):
+						 {
+					 		 }
+					 break;
+				 
+					 case (ladder):
+						 {
+					 		position =(position + dieThrow);
+						 }
+					 break;
+					 
+					 case (snake):
+						 {
+					 		position= (position - dieThrow);
+					 		
+					 		if (position<0)
+					 		{
+					 			position =(position + dieThrow);
+					 			
+					 			}
+					 		 }
+						 break;
+			
+					 }
+					
+					System.out.println("position after first throw : " + position );
+					
+					
+					
+					
+					
+					
+				}
+	
+	
+	
+	
+	
+	
+	
 
 }
